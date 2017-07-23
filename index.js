3f0519eccad36d0e2de2ef8c449fb9d44dbbe4b4
Bendegúz Attila Gellén <bndgzglln@bndgzglln.local>
2017-07-23 12:22:43 +0200

    update package.json

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ app.use(bodyParser());
 app.use(function (ctx, next) {
   return bot(ctx.request.body.message)
   .then(function (answere) {
-    console.log(answere);
+    console.log("API Response recieved.");
     ctx.body = answere;
     next();
   });
 });
 
-app.listen(3000);
+app.listen(process.env.PORT ||3000);
 
 
 
