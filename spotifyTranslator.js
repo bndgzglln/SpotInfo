c88c31a5de6f796728400ecbfcbdb28242e532d9
Bendegúz Attila Gellén <bndgzglln@bndgzglln.local>
2017-07-23 23:06:06 +0200

    added translation

--- spotifyTranslator.js
+++ spotifyTranslator.js
@@ -1,6 +1,6 @@
 const KEYMAP = ['C', 'C♯/D♭', 'D', 'D♯/E♭', 'E', 'F', 'F♯/G♭', 'G', 'G♯/A♭', 'A', 'A♯/B', 'H'];
 const MODE = ['Minor', 'Major'];
-const SIGNATURE = ['Leider nicht bekannt', '1/4', '1/2', '3/4', '4/4'];
+const SIGNATURE = ['Leider nicht bekannt', '1/4', '1/2', '3/4', '4/4', '5/4'];
 const TRANSLATORMAP = {
   key: function (value)  {
     return 'Tonart: ' + KEYMAP[value];
